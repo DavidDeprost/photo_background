dd023e565ba7187fe4d68841f4280a0487b1a712
Laurens Deprost <ldeprost@gmail.com>
2018-08-02 19:36:23 +0200

    Moved text strings over to constants.py

--- constants.py
+++ constants.py
@@ -11,3 +11,18 @@ API_KEY = \
 API_URL = 'https://api.unsplash.com/photos/random'
 DEFAULT_PHOTO_SUBJECT = 'sunset'
 FILENAME_LENGTH = 10
+
+# Info strings
+DESCRIPTION = ('Command line tool to automatically set a beautiful photograph '
+               'as desktop background.')
+SUBJECT_DESCRIPTION = 'The subject of the photograph'
+
+# Text message constants
+TEXT_ERROR_CONNECT = 'Error connecting to Unsplash.'
+TEXT_ERROR_DOWNLOAD = 'Error getting photo.'
+TEXT_ERROR_OS_UNSUPPORTED = 'System {} is not supported.'
+TEXT_ERROR_PING = 'Error while pinging download location.'
+TEXT_INFO_ATTRIBUTION = 'Photograph by {} on Unsplash.'
+TEXT_INFO_DOWNLOADING = 'Downloading photo...'
+TEXT_INFO_PICTURE_SET = 'Set photo as desktop background.'
+TEXT_INFO_SAVE_PATH = 'Saved the photo to {}.'
--- photo_background.py
+++ photo_background.py
@@ -18,14 +18,15 @@ def string_gen(size=constants.FILENAME_LENGTH, chars=string.ascii_lowercase + st
     return ''.join(random.choice(chars) for _ in range(size))
 
 def main(subject):
-    print('Downloading photo ...')
+    print(constants.TEXT_INFO_DOWNLOADING)
+
     params = {'orientation': 'landscape', 'query': subject}
     headers = {'Authorization': 'Client-ID ' + constants.API_KEY,
                 'Accept-Version': 'v1'}
 
     resp = requests.get(constants.API_URL, params=params, headers=headers)
     if not resp.status_code == 200:
-        print('Error connecting to unsplash.')
+        print(constants.TEXT_ERROR_CONNECT)
         return 1
     json = resp.json()
     # from pprint import pprint; pprint(json)
@@ -33,7 +34,7 @@ def main(subject):
     pic_url = json['urls']['full']
     resp = requests.get(pic_url)
     if not resp.status_code == 200:
-        print('Error getting photo.')
+        print(constants.TEXT_ERROR_DOWNLOAD)
         return 1
     ext = resp.headers['content-type'].split('/')[1]
     filename = string_gen() + '.' + ext
@@ -42,18 +43,17 @@ def main(subject):
     i = Image.open(BytesIO(resp.content))
     i.save(filename, ext)
     filepath = os.path.realpath(filename)
-    print('Saved the photo to', filepath)
+    print(constants.TEXT_INFO_SAVE_PATH.format(filepath))
     set_background(filepath)
 
     resp = requests.get(url, headers=headers)
     if not resp.status_code == 200:
-        print('Error while pinging download location.')
+        print(constants.TEXT_ERROR_PING)
         return 1
     # print('Pinged download location per api guidelines.')
 
     username = json['user']['name']
-    attribution = "Photograph by {} on Unsplash.".format(username)
-    print(attribution)
+    print(constants.TEXT_INFO_ATTRIBUTION.format(username))
 
     return 0
 
@@ -62,11 +62,11 @@ def set_background(filepath):
     opsys = platform.system()
     if opsys == 'Windows':
         ctypes.windll.user32.SystemParametersInfoW(20, 0, filepath, 0)
-        print('Set photo as desktop background.')
+        print(constants.TEXT_INFO_PICTURE_SET)
     elif opsys == 'Linux':
         command = 'gsettings set org.gnome.desktop.background picture-uri {}'.format(filepath)
         os.system(command)
-        print('Set photo as desktop background.')
+        print(constants.TEXT_INFO_PICTURE_SET)
     elif opsys == 'Darwin':
         SCRIPT = """/usr/bin/osascript<<END
 tell application "Finder"
@@ -74,14 +74,14 @@ set desktop picture to POSIX file "%s"
 end tell
 END"""
         subprocess.Popen(SCRIPT%filepath, shell=True)
-        print('Set photo as desktop background.')
+        print(constants.TEXT_INFO_PICTURE_SET)
     else:
-        print('OS not supported.')
+        print(constants.TEXT_ERROR_OS_UNSUPPORTED.format(opsys))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Command line tool to automatically set a beautiful photograph as desktop background.')
-    parser.add_argument('subject', help='The subject of the photograph', nargs='?', default=constants.DEFAULT_PHOTO_SUBJECT)
+    parser = argparse.ArgumentParser(description=constants.DESCRIPTION)
+    parser.add_argument('subject', help=constants.SUBJECT_DESCRIPTION, nargs='?', default=constants.DEFAULT_PHOTO_SUBJECT)
     args = parser.parse_args()
 
     main(args.subject)
